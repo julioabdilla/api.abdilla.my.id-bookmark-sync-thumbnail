50d5c059d0f612b118f8a0d170fe4d2752282b3b
julio.abdilla@gmail.com <julio.abdilla@gmail.com>
2025-01-11 16:26:18 +0700

    fix config service

--- main.ts
+++ main.ts
@@ -6,7 +6,7 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
+  const configService = app.get<ConfigService>(ConfigService);
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.KAFKA,
     options: {
